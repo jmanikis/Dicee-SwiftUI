a29830611692fde4bc07333f5e1716552f7c39e7
JC Manikis <jmanikis@icloud.com>
2021-06-30 12:38:08 -0400

    Complete Dicee App using SwiftUI

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("diceeLogo")
+                
+                Spacer()
+                
+                HStack {
+                    DiceView(number: leftDiceNumber)
+                    DiceView(number: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let number: Int
+    
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
             .padding()
     }
 }
@@ -17,5 +61,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11")
     }
 }
+
+
